ccd0d248e1859d022a09871e16d218debcb6319e
Billy Pinkerton <billybattlebones@gmail.com>
2022-06-14 19:07:46 -0700

    updating models again

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Hike = require('./Hike');
+
+const Boulder = require('./Boulder');
+
+const User = require('./User');
+
+module.exports = {Hike};
+
+module.exports = {Boulder};
+
+module.exports = {User};
