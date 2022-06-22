b6ba316c6133b6ccd1bee01457a58269b44c4aae
Justin Ellis <jdellis490@gmail.com>
2022-06-21 17:57:51 -0700

    New edits

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,49 +1,6 @@
 const router = require('express').Router();
 const User = require('../../models/User');
 
-<<<<<<< HEAD
-router.post('/login', async (req, res) => {
-  try {
-    const logData = await User.findOne({ where: { email: req.body.email } });
-
-    if (!logData) {
-      res
-        .status(400)
-        .json({ message: 'Login failed, please try a different email or password' });
-      return;
-    }
-
-    const correctPass = await logData.checkPassword(req.body.password);
-
-    if (!correctPass) {
-      res
-        .status(400)
-        .json({ message: 'Login failed, please try a different email or password' });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = logData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: logData, message: 'Welcome!' });
-    });
-
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
-});
-=======
 // router.get('/', async (req, res) => {
 //     const dataUser = await User.findAll().catch((err) => {
 //         res.json(err);
@@ -117,6 +74,5 @@ router.post('/', async (req, res) => {
       res.status(404).end();
     }
   });
->>>>>>> 5304b02a2d25b66dac3e444b0f5e80551d5e718d
 
 module.exports = router;
